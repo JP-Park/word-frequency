54fc3af4eac9ebc4fcec068ca62ac4b4a4f0c1c0
jp <jp.park@me.com>
2015-12-08 16:07:12 -0800

    final version

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,8 @@
 import re
 from collections import Counter
 
-# def word_frequency(file):
 with open("sample.txt") as sample:
         words = re.findall('[a-z]+', sample.read().lower())
         word_count = Counter(words).most_common(20)
-        print(word_count)
-
-
-# text = sample.read().lower()
-        # for line in text.split():
-        #     print(line)
+        for key, value in word_count:
+            print(key, value)
